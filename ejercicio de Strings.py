f2d009bb66edaeed5efc3e135a039d7e6b337352
Robertabreu23 <abreudelgado2@gmail.com>
2024-07-05 19:10:47 -0400

    Add files via upload

---
+++ ejercicio de Strings.py
@@ -0,0 +1,12 @@
+#Leer un texto y realizar las siguientes operaciones:
+s1=input("ingrese un texto: ")
+#Retornar el texto en mayusculas
+print(s1.upper())
+#Retornar el texto en minusculas
+print(s1.lower())
+#Retornar los dos primeros caracteres
+print(s1[0:2])
+#Retornar la cantidaad de veces que se repite el ultimo caracter
+print(s1.count(s1[-1]))
+#Retornar la longitud del texto
+print(len(s1))
